374cca80867fa22f54572e205420dd8b13d94082
Nitesh <niteshsaini1296@gmail.com>
2025-03-13 17:13:30 +0530

    License Added

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -11,7 +11,7 @@ const Footer = () => {
         <div className="footer-section">
           <h3>Company</h3>
           <p className="company-name">Mercantile Pay</p>
-       
+          <p className="company-license">License Number : 3489945</p>
         </div>
 
         {/* Quick Links */}
